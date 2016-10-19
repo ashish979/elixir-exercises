d96ad9d0642afaca0ee1510ddfc555c1ef00b639
Ashish Agrawal <ashishagrawal979@gmail.com>
2016-10-19 23:11:29 +0530

    first elixir program

---
+++ 1.exs
@@ -0,0 +1,19 @@
+defmodule Math do
+  def sum(a, b) do
+    a + b
+  end
+
+  def difference(a, b) do
+    a - b
+  end
+
+  def multiply(a, b) do
+    a * b
+  end
+
+  def division(a, b) do
+    a / b
+  end
+end
+
+IO.puts Math.sum(10, 20)
